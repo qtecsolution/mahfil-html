a1b2a5c22cec1e26d10a76d3668e74c6bbdca765
it27427 <istiak.tushar.qtec@gmail.com>
2022-11-29 12:59:01 +0600

    authentication completed

--- assets/js/search-suggestion/desksuggestion.js
+++ assets/js/search-suggestion/desksuggestion.js
@@ -1,5 +1,5 @@
 const input = document.querySelector("#desktopsearch-field");
-const suggestion = document.querySelector(".desktop-suggestions");
+const suggestion = document.querySelector(".desktopsuggestion");
 
 input.addEventListener("keyup", (e) => {
   let suggestionData = e.target.value;
