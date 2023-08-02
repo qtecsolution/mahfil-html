262664e06ef0e2e0fecdcf8b508d94dce88b3dba
it27427 <istiak.tushar.qtec@gmail.com>
2023-08-02 18:36:53 +0600

    timepicker updated

---
+++ assets/js/timepicker/mytimepicker.js
@@ -0,0 +1,11 @@
+$('.timepicker').timepicker({
+	timeFormat: 'h:mm p',
+	interval: 15,
+	minTime: '24',
+	maxTime: '11:59pm',
+	defaultTime: '24',
+	startTime: '24:00',
+	dynamic: false,
+	dropdown: true,
+	scrollbar: false
+});
