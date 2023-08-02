e0e2627652fb6a2f4552c2643de7b89943bbf018
it27427 <istiak.tushar.qtec@gmail.com>
2023-08-02 16:52:31 +0600

    insertpublish and schedulepublish page done

---
+++ assets/js/nice-select/insartpublish.js
@@ -0,0 +1,14 @@
+const videotype = NiceSelect.bind(document.getElementById("videotype"), {
+  searchable: false,
+  placeholder: 'Select video type'
+});
+
+const videostatus = NiceSelect.bind(document.getElementById("videostatus"), {
+  searchable: false,
+  placeholder: 'Select video status'
+});
+
+const publishingoption = NiceSelect.bind(document.getElementById("publishingoption"), {
+  searchable: false,
+  placeholder: 'Scheduled for later'
+});
